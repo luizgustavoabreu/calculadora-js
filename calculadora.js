afcdb9e5d6e2fd413dfff09a566d32edeecd9585
Luiz Gustavo <72631018+luizgustavoabreu@users.noreply.github.com>
2022-02-17 18:10:47 -0300

    Update

--- calculadora.js
+++ calculadora.js
@@ -1,9 +1,53 @@
 function calculadora() {
-  const operacao = prompt('Escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n 4 - Divisão real (/)\n 5 - Divisão inteira (%)\n 6 - Potenciação (**)');
+  const operacao = Number(prompt('Escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n 4 - Divisão real (/)\n 5 - Divisão inteira (%)\n 6 - Potenciação (**)'));
 
-let n1 = prompt('Insira o primeiro valor:');
-let n2 = prompt('Insira o segundo valor:');
+let n1 = Number(prompt('Insira o primeiro valor:'));
+let n2 = Number(prompt('Insira o segundo valor:'));
 let resultado;
+
+function soma() {
+  resultado = n1 + n2;
+  alert(`${n1} + ${n2} = ${resultado}`);
+  novaOperacao();
+}
+function subtracao() {
+  resultado = n1 - n2;
+  alert(`${n1} - ${n2} = ${resultado}`);
+  novaOperacao();
+}
+function multiplicacao() {
+  resultado = n1 * n2;
+  alert(`${n1} * ${n2} = ${resultado}`);
+  novaOperacao();
+}
+function divisaoReal() {
+  resultado = n1 / n2;
+  alert(`${n1} / ${n2} = ${resultado}`);
+  novaOperacao();
+}
+function divisaoInteira() {
+  resultado = n1 % n2;
+  alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`);
+  novaOperacao();
+}
+function potenciacao() {
+  resultado = n1 ** n2;
+  alert(`${n1} elevado a ${n2}ª é igual a ${resultado}`);
+  novaOperacao();
+}
+
+function novaOperacao() {
+  let opcao = prompt('Deseja fazer outra operção?\n 1 - Sim\n 2 - Não');
+
+  if (opcao == 1) {
+    calculadora();
+  } else if (opcao == 2) {
+      alert('Até mais!');
+  } else {
+      alert('Digite uma opção válida!');
+      novaOperacao();
+  }
+}
 
 if (operacao == 1) {
   soma();
